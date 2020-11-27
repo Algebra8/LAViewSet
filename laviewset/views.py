92190d38baa4643bf426b153c575bcf4e06e65a2
Algebra8 <mmnasrollahi@ucdavis.edu>
2020-11-27 12:55:25 -0800

    Minor refactoring due to routes changes.

--- laviewset/views.py
+++ laviewset/views.py
@@ -233,7 +233,7 @@ class ViewSignatureError(TypeError):
 
 empty = object()
 _fake_app = cast(web.UrlDispatcher, object())
-_fake_route = Route('', _fake_app)
+_fake_route = Route.create_base(_fake_app)
 
 
 class _ViewSet:
