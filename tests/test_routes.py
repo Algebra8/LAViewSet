5f3e48907f1cc33baf68018a14c43e8c0bd3fbc5
Algebra8 <mmnasrollahi@ucdavis.edu>
2020-11-27 19:45:27 -0800

    include tests for enforcement functionality.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from typing import cast
 
 import pytest
+import mock
 from aiohttp import web
 
-from laviewset import routes
+from laviewset import routes, rfc
+from laviewset.resources import Rfc
 
 
 _PATH = '/'
@@ -94,3 +96,72 @@ def test_wrapping(base_router):
     assert view_attrs.path == test_route.path + ''
     assert view_attrs.method == _METHOD
     assert view_attrs.routedef_kwargs == _ROUTEDEF_KWARGS
+
+
+@pytest.fixture
+def strict_route():
+    return routes.Route.create_base(
+        _fake_router, enforce=rfc.strict)
+
+
+@pytest.fixture
+def non_strict_route():
+    return routes.Route.create_base(
+        _fake_router, enforce=rfc.non_strict)
+
+
+@mock.patch('laviewset.routes.ViewAttrs')
+def test_enforce_view(mocked_view_attrs, strict_route, view):
+    # Our use of rfc.collection is somewhat arbitrary, but also
+    # let's us see that the enforcement gets to the view.
+    col_resource = strict_route('/listings', 'GET', res_type=rfc.collection)
+    _ = col_resource(view)
+    mocked_view_attrs.assert_called_once_with(
+        '/listings/', 'GET', {}
+    )
+
+
+def test_strict_view_subordinate(strict_route, view):
+    sub_resource = strict_route('pk', 'GET', res_type=rfc.subordinate)
+    sub_view = sub_resource(view)
+    sub_view_attrs = routes.get_view_attrs(sub_view)
+    assert sub_view_attrs.path == '/pk'
+
+
+def test_strict_res_type_fail(strict_route):
+    """Test the failing of a strictly enforced view.
+    """
+    with pytest.raises(routes.RouteError):
+        strict_route('/listings', 'GET')  # enforce not set
+
+
+def test_non_strict_res_type_fail(non_strict_route):
+    with pytest.raises(routes.RouteError):
+        non_strict_route(
+            '/listings', 'GET',
+            res_type=rfc.strict     # res_type should not be set with
+        )                           # NON-STRICT
+
+
+def test_enforce_extension(strict_route, non_strict_route):
+    # Strict extensions
+    s_extended_strict = strict_route.extend('listings', enforce=rfc.strict)
+    s_extended_non_strict = strict_route.extend(
+        'listings', enforce=rfc.non_strict)
+
+    assert Rfc(s_extended_strict._path.enforce) is Rfc.STRICT
+    assert Rfc(s_extended_non_strict._path.enforce) is Rfc.NON_STRICT
+
+    # Non-strict extensions
+    ns_s_extended_strict = non_strict_route.extend(
+        'listings', enforce=rfc.strict)
+    ns_extended_non_strict = non_strict_route.extend(
+        'listings', enforce=rfc.non_strict)
+
+    assert Rfc(ns_s_extended_strict._path.enforce) is Rfc.STRICT
+    assert Rfc(ns_extended_non_strict._path.enforce) is Rfc.NON_STRICT
+
+
+def test_enforce_create_base(strict_route, non_strict_route):
+    assert Rfc(strict_route._path.enforce) is Rfc.STRICT
+    assert Rfc(non_strict_route._path.enforce) is Rfc.NON_STRICT
