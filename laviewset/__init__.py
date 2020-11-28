6aed4c72914d2526265fb09eba99239c90e79aed
Algebra8 <mmnasrollahi@ucdavis.edu>
2020-11-27 19:45:46 -0800

    include interface for Rfc.

--- laviewset/__init__.py
+++ laviewset/__init__.py
@@ -2,11 +2,13 @@ from typing import Tuple
 
 from .routes import Route
 from .views import ViewSet, HttpMethods
+from .resources import rfc
 
 __all__: Tuple[str] = (
     'Route',
     'ViewSet',
-    'HttpMethods'
+    'HttpMethods',
+    'rfc'
 )
 
 __version__ = "0.0.1"
