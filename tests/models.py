605ef220510ad3157e36c6b7ffe8f4118cd82009
Milad Nasr <miald@lyte.com>
2020-12-13 15:01:27 -0800

    gino finish tests for model view set.

---
+++ tests/models.py
@@ -0,0 +1,42 @@
+import os
+
+from gino import Gino
+from marshmallow import Schema, fields
+from laviewset import SerializerMixin
+
+
+DB_ARGS = dict(
+    host=os.getenv("DB_HOST", "localhost"),
+    port=os.getenv("DB_PORT", 5432),
+    user=os.getenv("DB_USER", "postgres"),
+    password=os.getenv("DB_PASS", ""),
+    database=os.getenv("DB_NAME", "postgres"),
+)
+PG_URL = "postgresql://{user}:{password}@{host}" \
+         ":{port}/{database}".format(**DB_ARGS)
+
+db = Gino()
+
+
+class User(db.Model):
+
+    __tablename__ = "test_users"
+
+    id = db.Column(db.BigInteger(), primary_key=True)
+    nickname = db.Column("name", db.Unicode(), default=lambda: "test user")
+
+
+class UserSchema(Schema, SerializerMixin):
+
+    id = fields.Int(required=True)
+    nickname = fields.Str(required=True)
+
+    async def is_valid(self, cleaned_data, *args, **kwargs) -> None:
+        user = await User.select('id').where(
+            User.nickname == cleaned_data["nickname"]
+        ).gino.first()
+        if user is not None:
+            self.not_valid(
+                msg=f"User with nickname \"{cleaned_data['nickname']}\" "
+                    f"already exists."
+            )
---
+++ tests/test_model_views.py
@@ -0,0 +1,94 @@
+import json
+
+import pytest
+
+from laviewset import ModelViewSet
+from .models import User, UserSchema
+
+_serializer_class = UserSchema
+
+
+@pytest.fixture
+def model_viewset_core(db_router):
+
+    # app, router = db_router
+
+    class SomeViewSet(ModelViewSet):
+
+        route = db_router.extend('users')
+        model = User
+        serializer_class = UserSchema
+
+    return SomeViewSet
+
+
+@pytest.fixture
+def db_cli_core(loop, aiohttp_client, db_app, model_viewset_core):
+    return loop.run_until_complete(aiohttp_client(db_app))
+
+
+async def test_list(db_cli_core, get_all_users):
+    resp = await db_cli_core.get('/users')
+    assert resp.status == 200
+
+    dat = await resp.json()
+    assert dat == _serializer_class(many=True).dump(get_all_users)
+
+
+async def test_retrieve(db_cli_core, get_user_1):
+    resp = await db_cli_core.get('/users/1')
+    assert resp.status == 200
+
+    dat = await resp.json()
+    assert dat == _serializer_class().dump(get_user_1)
+
+
+async def test_create(db_cli_core):
+    data = {'id': 4, 'nickname': 'new_user'}
+    resp = await db_cli_core.post('/users', data=json.dumps(data))
+    assert resp.status == 201
+
+    dat = await resp.json()
+    assert dat['id'] == data['id']
+    assert dat['nickname'] == 'new_user'
+
+    assert resp.headers['Location'] == f"{resp.url}/{data['id']}"
+
+
+async def test_delete(db_cli_core):
+    resp = await db_cli_core.delete('/users/1')
+    assert resp.status == 204
+
+    attempt_get = await db_cli_core.get('/users/1')
+    assert attempt_get.status == 404
+
+
+async def test_partial_update(db_cli_core):
+    data = {'nickname': 'patched_nickname'}
+    resp = await db_cli_core.patch('/users/1', data=json.dumps(data))
+    assert resp.status == 200
+
+    patched_attempt = await db_cli_core.get('/users/1')
+    patched = await patched_attempt.json()
+    assert patched['nickname'] == data['nickname']
+
+
+async def test_update(db_cli_core):
+    partial_data = {'nickname': 'patched_nickname'}
+    bad_request_resp = await db_cli_core.put(
+        '/users/1',
+        data=json.dumps(partial_data)
+    )
+    assert bad_request_resp.status == 400
+
+    full_data = {'id': 1, 'nickname': 'patched_nickname'}
+    ok_resp = await db_cli_core.put(
+        '/users/1',
+        data=json.dumps(full_data)
+    )
+    assert ok_resp.status == 200
+
+    put_attempt = await db_cli_core.get('/users/1')
+    put = await put_attempt.json()
+    assert put['id'] == full_data['id']
+    assert put['nickname'] == full_data['nickname']
