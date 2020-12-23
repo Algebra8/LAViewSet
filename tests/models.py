af784c9261eedc58391fb021b78726471f9aeee1
Milad Nasr <miald@lyte.com>
2020-12-23 10:40:03 -0800

    update psql user.

--- tests/models.py
+++ tests/models.py
@@ -8,7 +8,7 @@ from laviewset import SerializerMixin
 DB_ARGS = dict(
     host=os.getenv("DB_HOST", "localhost"),
     port=os.getenv("DB_PORT", 5432),
-    user=os.getenv("DB_USER", "postgres"),
+    user=os.getenv("DB_USER", "laviewset"),
     password=os.getenv("DB_PASS", ""),
     database=os.getenv("DB_NAME", "postgres"),
 )
